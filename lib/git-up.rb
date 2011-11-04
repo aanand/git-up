088348c28c785efa8b59115c2b5cd5f33aafece0
Decklin Foster <decklin@red-bean.com>
2011-11-04 16:56:28 -0400

    Add a log hook command to show what changed

--- lib/git-up.rb
+++ lib/git-up.rb
@@ -52,6 +52,7 @@ class GitUp
         puts "rebasing...".yellow
       end
 
+      log(branch, remote)
       checkout(branch.name)
       rebase(remote)
     end
@@ -135,6 +136,12 @@ class GitUp
 
     unless on_branch?(branch_name)
       raise GitError.new("Failed to checkout #{branch_name}", output)
+    end
+  end
+
+  def log(branch, remote)
+    if log_hook = config("log-hook")
+      system('sh', '-c', log_hook, 'git-up', branch.name, remote.name)
     end
   end
 
